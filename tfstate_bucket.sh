74f924439f619cd00dd14eac0abfa8e613b9c318
Shilu <shilu842003@gmail.com>
2023-09-10 06:50:16 +0530

    added workflow code to run the api-server workflow

--- tfstate_bucket.sh
+++ tfstate_bucket.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
-RESOURCE_GROUP_NAME=tfstate
-STORAGE_ACCOUNT_NAME=tfstate$RANDOM
-CONTAINER_NAME=tfstate
+while getopts r:s:c:l: flag
+do
+    case "${flag}" in
+        r) resource_group_name=${OPTARG};;
+        s) storage_account_name=${OPTARG};;
+        c) container_name=${OPTARG};;
+		l) location=${OPTARG};;
+    esac
 
+done 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location eastus
+az group create --name $resource_group_name --location $location
 
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob
+az storage account create --resource-group $resource_group_name --name $storage_account_name --sku Standard_LRS --encryption-services blob
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
+az storage container create --name $container_name --account-name $storage_account_name
+
+ACCOUNT_KEY=$(az storage account keys list --resource-group $resource_group_name --account-name $storage_account_name --query '[0].value' -o tsv)
+
+echo "$ACCOUNT_KEY"
--- ubuntu-server/packer.auto.pkrvars.hcl
+++
@@ -1,5 +0,0 @@
-az_region = "centralindia"
-az_resource_group = "reverse_image_search"      # Must already exist
-location = "Central India"
-packer_image_name = "Reverse_Search_Engine_Server_1"
-vm_size = "Standard_B1s"
