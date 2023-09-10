6e1511c17efc5cbb8c194a01240e51407587feb7
adminuser <adminuser@kali.mvm>
2023-09-10 07:08:46 +0530

    added terraform code for creating containers

--- infrastructure/buckets/main.tf
+++ infrastructure/buckets/main.tf
@@ -1,13 +1,12 @@
-resource "azurerm_resource_group" "terraform_tg" {
-  name     = "example-resources"
-  location = var.location
+data "azurerm_resource_group" "rg" {
+  name = "reverse_image_search"
 }
 
 
 resource "azurerm_storage_account" "terraform_sa" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = var.account_tier
   account_replication_type = var.replication_type
 
---
+++ infrastructure/buckets/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = ">=0.12"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstate6apiapi86"
+      container_name       = "buckets-tfstate"
+      key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
